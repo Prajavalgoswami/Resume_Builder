5f6d89c6f1ccca30b2a51ec5d8821c9998c18fe2
Prajaval goswami <158766957+Prajavalgoswami@users.noreply.github.com>
2025-11-09 17:45:24 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongooose from 'mongoose';
 export const connectDB = async () => {
   try {
     await mongooose.connect(
-      'mongodb+srv://prajaval03:NhdxoPZdrfdGy8iV@cluster0.hvpghay.mongodb.net/ResumeBuilder?retryWrites=true&w=majority&appName=Cluster0'
+      'MONGO_URI'
     );
     console.log('MongoDB connected successfully');
   } catch (error) {
